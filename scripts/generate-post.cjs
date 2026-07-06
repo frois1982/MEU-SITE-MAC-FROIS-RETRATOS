08c42ee8e9f83d4976271205862a2e2bd4834850
frois1982 <frois.mac@gmail.com>
2026-07-06 11:42:04 -0300

    Blog: substitui imagens Unsplash por fotos reais do Cloudinary com crop inteligente no rosto

--- scripts/generate-post.cjs
+++ scripts/generate-post.cjs
@@ -13,22 +13,22 @@ if (!ANTHROPIC_API_KEY) {
 }
 
 const IMAGENS = [
-  'https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=1200',
-  'https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?w=1200',
-  'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=1200',
-  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=1200',
-  'https://images.unsplash.com/photo-1560250097-0b93528c311a?w=1200',
-  'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?w=1200',
-  'https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?w=1200',
-  'https://images.unsplash.com/photo-1556157382-97eda2f9e2bf?w=1200',
-  'https://images.unsplash.com/photo-1580489944761-15a19d654956?w=1200',
-  'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=1200',
-  'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=1200',
-  'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=1200',
-  'https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=1200',
-  'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=1200',
-  'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=1200'
-];
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/CORP_Empresario.jpg_21_bjqwpc',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/CORP_Empresario.jpg_8_nxkkrn',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/CORP_Empresario.jpg_15_HOME_m6bzke',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/PORT_RetratoMulher.jpg_tdfex5',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/PORT_RetratoMulher.jpg_4_k2hva7',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/PORT_RetratoMulher.jpg_15_rkn23u',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/ART_Conceito.jpg_7_m4gl6u',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/ART_Conceito.jpg_25_hku7p9',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/ART_Conceito.jpg_20_a45w8y',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/EU4A6541_ztrudm',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/EU4A6402_joygri',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/EU4A6476_bqdogp',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/13062026-EU4A8850_tud2ed',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/EU4A6345_jaf2v8',
+    'https://res.cloudinary.com/dlahvdclb/image/upload/q_auto,f_auto,w_1200,h_630,c_fill,g_face/1000057474.jpg_s86ppk',
+  ];
 
 const TOPICOS = [
   { titulo_base: 'Como uma boa foto de perfil pode aumentar suas vendas', keyword_principal: 'foto de perfil profissional', keywords: ['foto perfil linkedin', 'foto profissional florianopolis', 'retrato corporativo'], angulo: 'cases e dados concretos' },
